c7900b955c44823f2703f6b302d0ed9d80f8f95f
Daniel Garnier-Moiroux <git@garnier.wf>
2019-10-21 08:27:14 +0200

    Try and include iconv-lite

--- spec/spec.js
+++ spec/spec.js
@@ -1,4 +1,5 @@
 const { TextDecoder } = require('util');
+const iconv = require('iconv-lite');
 
 describe('text decoding =>', () => {
   it('fails', () => {
@@ -14,8 +15,25 @@ describe('text decoding =>', () => {
   });
 
   it('works with latin1', () => {
-    const decoder = new TextDecoder('latin-1');
-    const buffer = Buffer.from('houplà', 'latin-1');
+    const decoder = new TextDecoder('latin1');
+    const buffer = Buffer.from('houplà', 'latin1');
     expect(decoder.decode(buffer)).toEqual('houplà');
+  });
+});
+
+describe('iconv-lite =>', () => {
+  it('fails', () => {
+    const buffer = iconv.encode('houplà', 'latin1');
+    expect(iconv.decode(buffer, 'utf8')).not.toEqual('houplà');
+  });
+
+  it('works with utf8', () => {
+    const buffer = iconv.encode('houplà', 'utf8');
+    expect(iconv.decode(buffer, 'utf8')).toEqual('houplà');
+  });
+
+  it('works with latin1', () => {
+    const buffer = iconv.encode('houplà', 'latin1');
+    expect(iconv.decode(buffer, 'latin1')).toEqual('houplà');
   });
 });
